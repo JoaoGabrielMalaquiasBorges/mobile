fba5ac516e835903a91dc138fdb6d7e47a338ecd
João Gabriel Malaquias Borges <joaomalaquiassb@gmail.com>
2020-04-21 04:49:13 -0300

    36° Commit Modify package.json and yarn.lock files to restore vector icons library Format Profile.js file to put they in standard js style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect} from 'react';
-import { View, StatusBar, Text } from 'react-native';
-import { Video } from 'expo-av';
-import Icon from './CustomIcon';
-import * as Font from 'expo-font';
-import tweetObject from './Model';
+import React, { useState, useEffect } from 'react'
+import { View, StatusBar, Text } from 'react-native'
+import { Video } from 'expo-av'
+import Icon from './CustomIcon'
+import * as Font from 'expo-font'
+import tweetObject from './Model'
 
-function Profile({ route, navigation }) {
-    Font.loadAsync({
-        'FontName': require("../../assets/fonts/icomoon.ttf")
-    });
+/* global alert */
 
-    const tweet = tweetObject;
-    const notFullscreenSizeVideo = route.params;
-    const fullscreenVideoRef = React.createRef();
+function Profile ({ route, navigation }) {
+  Font.loadAsync({
+    FontName: require('../../assets/fonts/icomoon.ttf')
+  })
+  
+  const tweet = tweetObject
+  const notFullscreenSizeVideo = route.params
+  const fullscreenVideoRef = React.createRef()
 
-    //alert(notFullscreenSizeVideo.routeKey + '\n' + navigation.state.key)
+  // alert(notFullscreenSizeVideo.routeKey + '\n' + navigation.state.key)
 
-    useEffect(() => {
-        //alert( fullscreenVideoRef.current.props.source.uri );
-        /* fullscreenVideoRef.current.setStatusAsync({ positionMillis: 26000 }).then(
+  useEffect(() => {
+    // alert( fullscreenVideoRef.current.props.source.uri );
+    /* fullscreenVideoRef.current.setStatusAsync({ positionMillis: 26000 }).then(
             e => {
                 alert( JSON.stringify(notFullscreenSizeVideo.playbackStatus) );
             }
         ); */
-        return () => {
-            fullscreenVideoRef.current.unloadAsync();
-        };
-    });
+    return () => {
+      fullscreenVideoRef.current.unloadAsync()
+    }
+  })
 
-    /* for(var property in navigation.state) {
+  /* for(var property in navigation.state) {
         alert(property + "=" + navigation.state[property]);
     } */
-    
-    return(
-        <>
-            <StatusBar hidden={true} />
-            {/* <NavigationEvents
+
+  return (
+    <>
+      <StatusBar hidden />
+      {/* <NavigationEvents
                 onWillBlur={
                     payload => fullscreenVideoRef.current.getStatusAsync().then(
                         playbackStatus => {
@@ -45,31 +47,28 @@ function Profile({ route, navigation }) {
                     )
                 }
             /> */}
-            <View>
-                <Video
-                    ref={fullscreenVideoRef}
-                    resizeMode='contain'
-                    source={{ uri: tweet.extended_entities.media[0].video_info.variants[0].url /* 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' */}}
-                    style={{ height: 250, width: 300 }}
-                    status={notFullscreenSizeVideo.playbackStatus}
-                    //onPlaybackStatusUpdate={_onPlaybackStatusUpdate}
-                />
-                <Icon
-                    name='play'
-                    size={20}
-                    color="black"
-                    style={{ height: 20, marginLeft: 100 }}
-                    onPress={
-                        () => {
-                            fullscreenVideoRef.current.playAsync().then(
-                                alert('hi')
-                            );
-                        }
-                    }
-                />
-            </View>
-        </> 
-    );
+      <View>
+        <Video
+          ref={fullscreenVideoRef}
+          resizeMode='contain'
+          source={{ uri: tweet.extended_entities.media[0].video_info.variants[0].url /* 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' */ }}
+          style={{ height: 250, width: 300 }}
+          status={notFullscreenSizeVideo.playbackStatus}
+        />
+        <Icon
+          name='play'
+          size={20}
+          color='black'
+          style={{ height: 20, marginLeft: 100 }}
+          onPress={() => {
+            fullscreenVideoRef.current.playAsync().then(
+              alert('hi')
+            )
+          }}
+        />
+      </View>
+    </>
+  )
 }
 
-export default Profile;
+export default Profile
