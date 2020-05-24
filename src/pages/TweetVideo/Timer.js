57dec144712a77520a252bc230cd1e19283c5655
João Gabriel Malaquias Borges <joaomalaquiassb@gmail.com>
2020-05-24 03:01:17 -0300

    68° Commit Change Timer.js file by replacing millis to seconds and add minutes variable for that function start to be called at each second instead of each millisecond

--- src/pages/TweetVideo/Timer.js
+++ src/pages/TweetVideo/Timer.js
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { View, Dimensions, Animated,  TouchableWithoutFeedback, Image, Text  } from 'react-native'
 import { container, progressBar } from './TweetVideo/styles'
 
-var millis = -1
+var seconds = 0
+var minutes = 0
 var shouldIncrement = true
 const myEmitter = new EventEmitter()
 
 export function start () {
-    millis++
-    if (shouldIncrement) {
-        start()
-    }
+    seconds++
+    setInterval(() => {
+        myEmitter.emit('eventOne')
+        if (shouldIncrement) {
+            start()
+        }
+    }, 1000)
 }
 
-export function startAt (millis) {
-    millis = millis
-    if (shouldIncrement) {
-        start()
+export function startAt (seconds) {
+    seconds = seconds
+    setInterval(() => {
+        myEmitter.emit('eventOne')
+        if (shouldIncrement) {
+            start()
+        }
+    }, 1000)
 }
 
 export function stop () {
@@ -24,7 +32,11 @@ export function stop () {
 }
 
 function transform () {
-    
+    if (seconds % 60 == 1) {
+        seconds = 0
+        minutes++
+    }
+    setTime((minutes > 9 ? minutes : '0' + minutes) + ':' + (seconds > 9 ? seconds : '0' + seconds))
 }
 
 function Timer () {
