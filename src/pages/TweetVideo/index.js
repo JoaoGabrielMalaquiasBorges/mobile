9d4d59f9d2d6d748a0d7426e2193329d17ae73f5
João Gabriel Malaquias Borges <joaomalaquiassb@gmail.com>
2020-07-27 03:09:59 -0300

    113° Commit

--- src/pages/TweetVideo/index.js
+++ src/pages/TweetVideo/index.js
@@ -6,6 +6,7 @@ import ControlBar from "./ControlBar";
 import { reRenderPlayback } from './ControlBar/Controls/Playback';
 import { finishProgress, testFunction } from './ControlBar/ProgressBar';
 import { video } from './styles';
+import Icon from '../CustomIcon';
 
 /* Video.setAudioModeAsync({
     shouldDuckAndroid: false,
@@ -56,6 +57,31 @@ function TweetVideo(props) {
                 <Animated.View ref={controlBarWrapper} style={{ ...video.topLayer, opacity: displayValue, zIndex: displayValue }}>
                     <ControlBar route={props.route} navigation={props.navigation} videoRef={videoRef}/>
                 </Animated.View>
+                <View style={{
+                    height: '100%',
+                    width: '100%',
+                    position: 'absolute',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <View style={{
+                        height: 50,
+                        width: 50,
+                        borderWidth: 3,
+                        borderRadius: 30,
+                        borderColor: 'white',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#1DA1F2'
+                    }}>
+                        <Icon
+                            name='play'
+                            size={22}
+                            color='white'
+                            style={{ paddingLeft: 4 }}
+                        />
+                    </View>
+                </View>
                 <TouchableWithoutFeedback onPressIn={() => {
                     Animated.timing(displayValue, {
                         toValue: (flag-1)*(-1),
