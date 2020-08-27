c63aa58d9b411bcdbbe1d01e38999c2323648189
João Gabriel Malaquias Borges <joaomalaquiassb@gmail.com>
2020-08-27 02:02:17 -0300

    130° Commit

--- assets/loader.js
+++ assets/loader.js
@@ -2,32 +2,34 @@ export function loader () {
   return `
     <!DOCTYPE html>
     <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    .loader {
-      border: 2px solid #f3f3f3;
-      border-radius: 50%;
-      border-top: 2px solid #3498db;
-      border-left: 2px solid #3498db;
-      width: 50px;
-      height: 50px;
-      animation: spin 0.5s linear infinite;
-    }
-    
-    @keyframes spin {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
-    }
-    </style>
-    </head>
-    <body>
-    
-    <h2>How To Create A Loader</h2>
-    
-    <div class="loader"></div>
-    
-    </body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+          .loader {
+            border: 2px solid #f3f3f3;
+            border-radius: 50%;
+            border-top: 2px solid #3498db;
+            width: 40px;
+            height: 40px;
+            animation: spin 0.5s linear infinite;
+          }
+
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        </style>
+      </head>
+      <body>
+        <div style="height: 44px; width: 44px; margin: auto">
+          <div style="height: 44px; width: 44px; position: absolute; display: flex; justify-content: center; align-items: center">
+            <div style="height: 22px; width: 22px">
+              <svg id="Logo_FIXED" data-name="Logo — FIXED" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400"><defs><style>.cls-1{fill:none;}.cls-2{fill:#1da1f2;}</style></defs><title>Twitter_Logo_Blue</title><rect class="cls-1" width="400" height="400"/><path class="cls-2" d="M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23"/></svg>
+            </div>
+          </div>
+          <div class="loader"/>
+        </div>
+      </body>
     </html>
   `
 }
